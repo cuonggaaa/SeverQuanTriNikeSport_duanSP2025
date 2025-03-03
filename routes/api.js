3e3b223a1e48e52020b8ec2c00845caa1e66d074
kydhph24886 <kydo648@gmail.com>
2025-03-03 22:03:25 +0700

    cap nhat route api

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,12 @@ const router = express.Router();
 
 const apiBannerRoute = require('./api.banner.js');
 const apiUserRoute = require('./api.user.js');
-
+const apiCategoryRoute = require('./api.category.js');
+const apiProductRoute = require('./api.product.js');
 
 router.use('/banner', apiBannerRoute);
 router.use('/user', apiUserRoute);
-
+router.use('/category', apiCategoryRoute);
+router.use('/product', apiProductRoute);
 
 module.exports = router;
