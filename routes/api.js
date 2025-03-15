b5b50f092e19befda44b333afb27d0b72d254f6c
tiendtph23628 <dangthetien14@gmail.com>
2025-03-15 22:16:37 +0700

    add route api voucher

--- routes/api.js
+++ routes/api.js
@@ -6,10 +6,14 @@ const apiBannerRoute = require('./api.banner.js');
 const apiUserRoute = require('./api.user.js');
 const apiCategoryRoute = require('./api.category.js');
 const apiProductRoute = require('./api.product.js');
+const apiVoucherRoute = require('./api.voucher.js');
 
 router.use('/banner', apiBannerRoute);
 router.use('/user', apiUserRoute);
 router.use('/category', apiCategoryRoute);
 router.use('/product', apiProductRoute);
+router.use('/voucher', apiVoucherRoute);
+
+
 
 module.exports = router;
