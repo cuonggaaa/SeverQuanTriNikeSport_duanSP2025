42733e025eaced5bc903d75ed6429acf341e0c4a
tiendtph23628 <dangthetien14@gmail.com>
2025-03-15 22:14:01 +0700

    add route voucher

---
+++ routes/voucher.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { upload } = require('../service/upload.service');
+
+const api = require('../controllers/api.voucher');
+
+router.get('/', api.list);
+
+router.get('/add', api.add);
+router.post('/add', upload.single('image'), api.add);
+
+router.get('/edit/:id', api.edit);
+router.post('/edit/:id', upload.single('image'), api.edit);
+
+router.get('/del/:id', api.del);
+router.post('/del/:id', api.del);
+
+
+module.exports = router;
