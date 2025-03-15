3f79dc954a5b1abdb34cee8de088f1f24c6dbc0d
kydhph24886 <kydo648@gmail.com>
2025-03-15 21:28:20 +0700

    router user

---
+++ routes/user.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const { upload } = require('../service/upload.service');
+
+const api = require('../controllers/api.user');
+
+router.get('/', api.list);
+router.post('/', api.list);
+
+
+router.get('/add', api.add);
+router.post('/add', upload.single('image'), api.add);
+
+router.get('/edit/:id', api.edit);
+router.post('/edit/:id', upload.single('image'), api.edit);
+
+router.get('/del/:id', api.del);
+router.post('/del/:id', api.del);
+
+
+module.exports = router;
