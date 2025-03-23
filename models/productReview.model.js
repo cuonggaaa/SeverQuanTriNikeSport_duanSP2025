3c88517666e5add3bcca3222177ca7a88024a611
tiendtph23628 <dangthetien14@gmail.com>
2025-03-23 11:19:30 +0700

    update model

---
+++ models/productReview.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const APIError = require('../helpers/APIError');
+
+const productReviewSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'userModel',
+      required: true
+    },
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'productModel',
+      required: true
+    },
+    rating: { type: Number, required: true },
+    reviewText: { type: String, required: true }
+  },
+  { collection: 'product_reviews', timestamps: true }
+);
+
+productReviewSchema.statics = {
+  async get(id) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new APIError('Invalid ID format');
+    }
+
+    try {
+      const data = await this.findById(id).exec();
+      if (data) {
+        return data;
+      }
+      throw new APIError('No such data exists!');
+    } catch (error) {
+      throw new APIError(error.message || 'An error occurred during the query');
+    }
+  }
+};
+
+module.exports = mongoose.model('productReviewsModel', productReviewSchema);
