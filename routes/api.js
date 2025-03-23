e9a8318b996dd684fe82935b56572f194f2299da
cuongnvph24076 <cuonggnv1209@gmail.com>
2025-03-23 10:53:59 +0700

    update api route

--- routes/api.js
+++ routes/api.js
@@ -6,13 +6,19 @@ const apiBannerRoute = require('./api.banner.js');
 const apiUserRoute = require('./api.user.js');
 const apiCategoryRoute = require('./api.category.js');
 const apiProductRoute = require('./api.product.js');
+const apiPayMethodRoute = require('./api.payMethod');
 const apiVoucherRoute = require('./api.voucher.js');
+const apiCartRoute = require('./api.cart');
+const apiOrderRoute = require('./api.order');
 
 router.use('/banner', apiBannerRoute);
 router.use('/user', apiUserRoute);
 router.use('/category', apiCategoryRoute);
 router.use('/product', apiProductRoute);
+router.use('/pay-method', apiPayMethodRoute);
 router.use('/voucher', apiVoucherRoute);
+router.use('/cart', apiCartRoute);
+router.use('/order', apiOrderRoute);
 
 
 
