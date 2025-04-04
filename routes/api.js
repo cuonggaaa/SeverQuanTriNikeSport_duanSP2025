d9c4ddb898634def43b4d1459c67ac692822878b
kydhph24886 <kydo648@gmail.com>
2025-04-04 21:58:56 +0700

    update route

--- routes/api.js
+++ routes/api.js
@@ -2,24 +2,35 @@ const express = require('express');
 const router = express.Router();
 
 
-const apiBannerRoute = require('./api.banner.js');
-const apiUserRoute = require('./api.user.js');
-const apiCategoryRoute = require('./api.category.js');
-const apiProductRoute = require('./api.product.js');
+const apiBannerRoute = require('./api.banner');
+const apiUserRoute = require('./api.user');
+const apiCategoryRoute = require('./api.category');
+const apiProductRoute = require('./api.product');
+const apiProductRiviewRoute = require('./api.productReview');
+const apiProductFavRoute = require('./api.productFav');
 const apiPayMethodRoute = require('./api.payMethod');
-const apiVoucherRoute = require('./api.voucher.js');
+const apiVoucherRoute = require('./api.voucher');
 const apiCartRoute = require('./api.cart');
 const apiOrderRoute = require('./api.order');
+
+// const apiGradesRoute = require('./api.grades');
+// const apiTuitionPaymentRoute = require('./api.tuitionPayment');
+// const apiScholarshipRoute = require('./api.scholarship');
 
 router.use('/banner', apiBannerRoute);
 router.use('/user', apiUserRoute);
 router.use('/category', apiCategoryRoute);
 router.use('/product', apiProductRoute);
+router.use('/product-review', apiProductRiviewRoute);
+router.use('/product-fav', apiProductFavRoute);
 router.use('/pay-method', apiPayMethodRoute);
 router.use('/voucher', apiVoucherRoute);
 router.use('/cart', apiCartRoute);
 router.use('/order', apiOrderRoute);
 
+// router.use('/grades', apiGradesRoute);
+// router.use('/tuition', apiTuitionPaymentRoute);
+// router.use('/scholarship', apiScholarshipRoute);
 
 
 module.exports = router;
